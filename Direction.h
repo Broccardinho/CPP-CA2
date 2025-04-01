4180735603e495690889674d0e70fc0f1cb99b8a
AlexBroccardo <159144351+Broccardinho@users.noreply.github.com>
2025-04-01 12:12:44 +0100

    build(project):
    Direction.h and Position.h

--- Direction.h
+++ Direction.h
@@ -6,3 +6,10 @@
 #define DIRECTION_H
 
 #endif //DIRECTION_H
+
+enum class Direction {
+    NORTH = 1,
+    EAST = 2,
+    SOUTH = 3,
+    WEST = 4,
+};
--- Position.h
+++ Position.h
@@ -6,3 +6,12 @@
 #define POSITION_H
 
 #endif //POSITION_H
+
+struct Position {
+    int x;
+    int y;
+
+    bool operator==(const Position& other) const {
+        return x == other.x && y == other.y;
+    }
+};
